b2e9b0cf74f1e6c2665b39c6475b42f45829605c
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-03-24 16:26:31 +0000

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,25 @@
+local UI = game:GetObjects("rbxassetid://4814616806")[1]
+UI.Parent = game:GetService("CoreGui")
+
+local Frames = {
+	[UI.SelectFrame.RemoteSpyBtn] = UI.RemoteSpyFrame
+}
+
+for i, v in pairs(Frames) do
+	i.MouseButton1Click:Connect(function()
+		v.Visible = not v.Visible
+	end)
+end
+
+getgenv().UpdateRemote = function(name, icon)
+	if not UI.RemoteSpyFrame.FiredFrame[name] then
+		local Template = UI.RemoteSpyFrame.TemplateFrame:Clone()
+		Template.Name, Template.Parent, Template.Visible = name, UI.RemoteSpyFrame.FiredFrame, true
+		Template.Position = UDim2.new(0, 5, 0, 5 + 30 * #UI.RemoteSpyFrame.FiredFrame:GetChildren())
+		Template.ItemName.Text = name
+		Template.TypeImg.Image = icon
+	else
+		local Count = UI.RemoteSpyFrame.FiredFrame[name].CallCount
+		Count.Text = tostring(tonumber(Count.Text) + 1)
+	end
+end
