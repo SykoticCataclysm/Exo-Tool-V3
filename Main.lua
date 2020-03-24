acb4f5a3e36d4fe3bfd40e7ce53e330df72823ca
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-03-24 16:37:35 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,18 +1,6 @@
 local UI = game:GetObjects("rbxassetid://4814616806")[1]
 UI.Parent = game:GetService("CoreGui")
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SykoticCataclysm/ExoToolV2/master/RemoteSpy.lua"))()
-
-local Frames = {
-	[UI.SelectFrame.RemoteSpyBtn] = UI.RemoteSpyFrame
-}
-
-for i, v in pairs(Frames) do
-	i.MouseButton1Click:Connect(function()
-		v.Visible = not v.Visible
-	end)
-end
-
 getgenv().UpdateRemote = function(name, classname, icon, args)
 	if not UI.RemoteSpyFrame.FiredFrame[name] then
 		local Template = UI.RemoteSpyFrame.TemplateFrame:Clone()
@@ -24,4 +12,16 @@ getgenv().UpdateRemote = function(name, classname, icon, args)
 		local Count = UI.RemoteSpyFrame.FiredFrame[name].CallCount
 		Count.Text = tostring(tonumber(Count.Text) + 1)
 	end
+end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SykoticCataclysm/ExoToolV2/master/RemoteSpy.lua"))()
+
+local Frames = {
+	[UI.SelectFrame.RemoteSpyBtn] = UI.RemoteSpyFrame
+}
+
+for i, v in pairs(Frames) do
+	i.MouseButton1Click:Connect(function()
+		v.Visible = not v.Visible
+	end)
 end
