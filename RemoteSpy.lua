5605070843160b44d8d3df25c0598e527429d26d
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-03-24 22:34:53 +0000

    Update RemoteSpy.lua

--- RemoteSpy.lua
+++ RemoteSpy.lua
@@ -33,7 +33,6 @@ if game.PlaceId == 606849621 then
 						local Args = {...}
 						if jbArg == nil then
 							jbArg = Args[1]
-						elseif Args[1] == jbArg then
 							Fired[#Fired + 1] = { "JailBreak Spam Arg", "RemoteEvent", "rbxassetid://4229806545", {...} }
 						elseif Args[1] ~= jbArg then
 							Fired[#Fired + 1] = { "JailBreak Bypass", "RemoteEvent", "rbxassetid://4229806545", {...} }
