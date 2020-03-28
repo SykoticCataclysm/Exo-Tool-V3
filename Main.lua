745abd3e5dce5001751f2472720612db6d6deb34
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-03-28 19:05:23 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -91,6 +91,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/SykoticCataclysm/ExoT
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SykoticCataclysm/ExoToolV2/master/UpvScanner.lua"))()
 
 UI.UpvScanFrame.Main.Search.FocusLost:Connect(function()
+	UI.UpvScanFrame.Main.ScanResultsFrame:ClearAllChildren()
 	Scan(UI.UpvScanFrame.Main.Search.Text)
 end)
 
