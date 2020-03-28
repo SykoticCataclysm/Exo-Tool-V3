a25925596d1a4db534e8958e249328a500f6a964
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-03-28 18:53:29 +0000

    Update UpvScanner.lua

--- UpvScanner.lua
+++ UpvScanner.lua
@@ -1,9 +1,22 @@
+function CheckName(search, result)
+	local a = string.lower(search)
+	local b = string.lower(tostring(result))
+	if string.find(b, a) then
+		return true
+	end
+	return false
+end
+
 getgenv().Scan = function(name)
 	for a, b in next, getgc() do
 		if type(b) == "function" then
 			for c, d in next, debug.getupvalues(b) do
-				if type(d) == "table" and rawget(d, name) then
-					AddUpvalue(tostring(b), tostring(d), name, d[name])
+				if type(d) == "table" then
+					for e, f in next, d do
+						if CheckName(name, e) == true then
+							AddUpvalue(b, d, tostring(e), GetType(f))
+						end
+					end
 				end
 			end
 		end
