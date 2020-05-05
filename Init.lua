fe108d13db34777e68e2fefe9a74f38b11a017ec
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-05-05 17:26:00 +0100

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -1,1 +1,9 @@
+warn("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+warn("")
+warn("            ExoTool V3 - Made By Syko            ")
+warn("        Discord: discord.me/sykoscripting        ")
+warn("")
+warn("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/SykoticCataclysm/Exo-Tool-V3/master/ScriptDumper.lua"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/SykoticCataclysm/Exo-Tool-V3/master/GUI.lua"))()
