35eac11b06ce8248a89aaffd32f5a8493262def7
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-05-05 22:55:14 +0100

    Update RemoteSpy.lua

--- RemoteSpy.lua
+++ RemoteSpy.lua
@@ -2,8 +2,31 @@ getgenv().GetAllRemotes = function()
 	local Remotes = {}
 	for i, v in pairs(game:GetDescendants()) do
 		if v:IsA("BindableEvent") or v:IsA("BindableFunction") or v:IsA("RemoteEvent") or v:IsA("RemoteFunction") then
-			Remotes[v] = true
+			if not Remotes[v.Name] then
+				Remotes[v.Name] = { true, 0, nil, {} }
+			end
 		end
 	end
 	return Remotes
 end
+
+repeat wait() until getgenv().UpdateRemote
+
+local Remotes = { 
+	["BindableEvent"] = true,
+	["BindableFunction"] = true,
+	["RemoteEvent"] = true,
+	["RemoteFunction"] = true
+}
+
+local mt = getrawmetatable(game)
+local nc = mt.__namecall
+setreadonly(mt, false)
+
+mt.__namecall = function(inst, ...)
+	if Remotes[inst.ClassName] then
+		UpdateRemote(inst.Name, {...})
+	end
+end
+
+setreadonly(mt, true)
