c21340564c9889211d1c5eb745560de597aa4515
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-05-05 17:28:43 +0100

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -5,5 +5,5 @@ warn("        Discord: discord.me/sykoscripting        ")
 warn("")
 warn("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/SykoticCataclysm/Exo-Tool-V3/master/ScriptDumper.lua"))()
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/SykoticCataclysm/Exo-Tool-V3/master/GUI.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SykoticCataclysm/Exo-Tool-V3/master/ScriptDumper.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SykoticCataclysm/Exo-Tool-V3/master/GUI.lua"))()
