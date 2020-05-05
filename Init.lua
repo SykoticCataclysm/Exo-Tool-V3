6a8e9336b09f0d3a90e048443ba78d0af8edadee
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-05-05 22:08:53 +0100

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -5,5 +5,6 @@ warn("        Discord: discord.me/sykoscripting        ")
 warn("")
 warn("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SykoticCataclysm/Exo-Tool-V3/master/RemoteSpy.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SykoticCataclysm/Exo-Tool-V3/master/ScriptDumper.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SykoticCataclysm/Exo-Tool-V3/master/GUI.lua"))()
