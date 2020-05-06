69b27f56a49f30d9369a7a24716a9158cc389997
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-05-06 17:01:26 +0100

    Update RemoteSpy.lua

--- RemoteSpy.lua
+++ RemoteSpy.lua
@@ -1,1 +1,40 @@
+local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/SykoticCataclysm/Exo-Tool-V3/master/Gui.lua", true))()
 
+local RemoteEvents = { 
+	["Fire"] = true,
+	["FireServer"] = true,
+	["Invoke"] = true,
+	["InvokeServer"] = true
+}
+
+local HttpMethods = {
+	["HttpGet"] = true,
+	["HttpGetAsync"] = true,
+	["HttpPost"] = true,
+	["HttpPostAsync"] = true,
+	["GetObjects"] = true
+}
+
+local Remotes = {}
+
+local mt = getrawmetatable(game)
+local nc = mt.__namecall
+setreadonly(mt, false)
+
+mt.__namecall = function(inst, ...)
+	local Args = {...}
+	local Method = getnamecallmethod()
+	if RemoteEvents[Method] then
+		if not Remotes[inst] then
+			Remotes[inst] = true
+			ui.addremote(inst)
+		else
+			ui.updateremote(inst)
+		end
+	elseif HttpMethods[Method] then
+
+	end
+	return nc(inst, ...)
+end
+
+setreadonly(mt, true)
