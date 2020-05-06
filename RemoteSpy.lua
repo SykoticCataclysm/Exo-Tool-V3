fa583ce414bc4d00003101906ea70c97ea5b0505
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-05-06 17:11:47 +0100

    Update RemoteSpy.lua

--- RemoteSpy.lua
+++ RemoteSpy.lua
@@ -17,12 +17,15 @@ mt.__namecall = function(inst, ...)
 	local Args = {...}
 	local Method = getnamecallmethod()
 	if RemoteEvents[Method] then
+		local old = syn_context_get()
+		syn_context_set(6)
 		if not Remotes[inst] then
 			Remotes[inst] = true
 			ui.addremote(inst)
 		else
 			ui.updateremote(inst)
 		end
+		syn_context_set(old)
 	end
 	return nc(inst, ...)
 end
