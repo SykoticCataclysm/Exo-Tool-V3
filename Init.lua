34f820e1fb7fc8887e72b40aab94037cef975260
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-05-06 17:03:07 +0100

    Create Init.lua

---
+++ Init.lua
@@ -0,0 +1,8 @@
+warn("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+warn("")
+warn("            ExoTool V3 - Made By Syko            ")
+warn("        Discord: discord.me/sykoscripting        ")
+warn("")
+warn("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SykoticCataclysm/Exo-Tool-V3/master/RemoteSpy.lua"))()
