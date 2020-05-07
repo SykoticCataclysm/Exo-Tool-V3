b560336b2037a9e226933954cfebcad622fc1fed
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-05-07 17:38:42 +0100

    Update Compatibility.lua

--- Compatibility.lua
+++ Compatibility.lua
@@ -1,4 +1,4 @@
 getgenv().setclipboard = setclipboard or Clipboard.set
 getgenv().getnilinstances = getnilinstances or get_nil_instances
-getgenv().getrawmetatable or getrawmetatable or debug.getmetatable
+getgenv().getrawmetatable = getrawmetatable or debug.getmetatable
 getgenv().setreadonly = setreadonly or make_readonly
