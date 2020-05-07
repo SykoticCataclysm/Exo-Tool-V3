29ce4bd9568289a19533e18b45c7dae7ae950e97
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-05-07 19:35:46 +0100

    Update Compatibility.lua

--- Compatibility.lua
+++ Compatibility.lua
@@ -3,4 +3,5 @@ getgenv().getnilinstances = getnilinstances or get_nil_instances
 getgenv().getrawmetatable = getrawmetatable or debug.getmetatable
 getgenv().setreadonly = setreadonly or make_readonly
 getgenv().getgc = getgc or get_gc_objects
-getgenv().getupvalues = debug.getupvalues or getupvalues
+getgenv().getupvalues = debug.getupvalues or getupvalues or getupvals
+getgenv().isexploitclosure = is_synapse_function or is_protosmasher_caller or is_sirhurt_closure
