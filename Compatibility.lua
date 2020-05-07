78544ec9366456edc11bb71a033d1a15f498bab3
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-05-07 17:07:56 +0100

    Update Compatibility.lua

--- Compatibility.lua
+++ Compatibility.lua
@@ -1,2 +1,12 @@
 getgenv().setclipboard = setclipboard or Clipboard.set
 getgenv().getnilinstances = getnilinstances or get_nil_instances
+getgenv().getrawmetatable or getrawmetatable or debug.getmetatable
+getgenv().setreadonly = function(obj, bool)
+	if setreadonly then
+		setreadonly(obj, bool)
+	elseif make_readonly then
+		make_readonly(obj, bool)
+	elseif make_writeable then
+		make_writeable(obj, not bool)
+	end
+end
